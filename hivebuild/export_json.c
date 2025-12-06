7d72c77cc446ff7cd617059faab43b9921ad9aff
sprogma <mux-228@yandex.ru>
2025-12-06 18:48:24 +0300

    start export to json

--- hivebuild/export_json.c
+++ hivebuild/export_json.c
@@ -5,199 +5,232 @@
 #include "stdlib.h"
 
 
-static void print_type(struct type *t)
+static void print_type(FILE *f, struct type *t)
 {
+    fprintf(f, "\"%lld\":{", (int64_t)t);
+    fprintf(f, "\"type\":%d", t->type);
     switch (t->type)
     {
         case VAR_SCALAR:
-            printf("Scalar %s [size %lld] ", t->scalar.name, size_of_type(t));
+            fprintf(f, ",");
+            fprintf(f, "\"scalar\":\"%s\",", t->scalar.name);
+            fprintf(f, "\"size\":%lld", size_of_type(t));
             break;
         case VAR_CLASS:
-            printf("Class %s [size %lld] ", t->class.name, size_of_type(t));
+            fprintf(f, ",");
+            fprintf(f, "\"class:\"%s\",", t->class.name);
+            fprintf(f, "\"fields\":{");
+            for (int i = 0; i < t->class.record->record.fields_len; ++i)
+            {
+                if (i != 0) { putc(',', f); }
+                fprintf(f, "\"%s\":%lld", t->class.record->record.fields_name[i], 
+                                          (int64_t)t->class.record->record.fields_type[i]);
+            }
+            fprintf(f, "}");
             break;
         case VAR_RECORD:
-            printf("Record %s [size %lld] ", t->record.name, size_of_type(t));
+            fprintf(f, ",");
+            fprintf(f, "\"record\":%s,", t->record.name);
+            fprintf(f, "\"fields\":{");
+            for (int i = 0; i < t->record.fields_len; ++i)
+            {
+                if (i != 0) { putc(',', f); }
+                fprintf(f, "\"%s\":%lld", t->record.fields_name[i], 
+                                          (int64_t)t->record.fields_type[i]);
+            }
+            fprintf(f, "}");
             break;
         case VAR_ARRAY:
-            printf("Array [size %lld] of type ", size_of_type(t));
-            print_type(t->array.base);
+            fprintf(f, ",");
+            fprintf(f, "\"base\":%lld", (int64_t)t->array.base);
             break;
         case VAR_PIPE:
-            printf("Pipe [size %lld] of type ", size_of_type(t));
-            print_type(t->pipe.base);
+            fprintf(f, ",");
+            fprintf(f, "\"base\":%lld", (int64_t)t->array.base);
             break;
     }
+    fprintf(f, "}");
 }
 
 
-static void print_variable(struct variable *v)
+static void print_variable(FILE *f, struct variable *v)
 {
-    printf("Var %s of type: ", v->name);
-    print_type(v->type);
+    fprintf(f, "Var %s of type: ", v->name);
+    print_type(f, v->type);
 }
 
 
-static void print_op(enum op_type t)
+static void print_op(FILE *f, enum op_type t)
 {
     switch (t)
     {
-        case OP_ADD: printf(" ADD "); break;
-        case OP_SUB: printf(" SUB "); break;
-        case OP_MUL: printf(" MUL "); break;
-        case OP_DIV: printf(" DIV "); break;
-        case OP_MOD: printf(" MOD "); break;
+        case OP_ADD: fprintf(f, " ADD "); break;
+        case OP_SUB: fprintf(f, " SUB "); break;
+        case OP_MUL: fprintf(f, " MUL "); break;
+        case OP_DIV: fprintf(f, " DIV "); break;
+        case OP_MOD: fprintf(f, " MOD "); break;
 
-        case OP_AND: printf(" AND "); break;
-        case OP_NOT: printf(" NOT "); break;
-        case OP_OR: printf(" OR "); break;
-        case OP_XOR: printf(" XOR "); break;
+        case OP_AND: fprintf(f, " AND "); break;
+        case OP_NOT: fprintf(f, " NOT "); break;
+        case OP_OR: fprintf(f, " OR "); break;
+        case OP_XOR: fprintf(f, " XOR "); break;
 
-        case OP_BAND: printf(" BAND "); break;
-        case OP_BNOT: printf(" BNOT "); break;
-        case OP_BOR: printf(" BOR "); break;
-        case OP_BXOR: printf(" BXOR "); break;
+        case OP_BAND: fprintf(f, " BAND "); break;
+        case OP_BNOT: fprintf(f, " BNOT "); break;
+        case OP_BOR: fprintf(f, " BOR "); break;
+        case OP_BXOR: fprintf(f, " BXOR "); break;
 
-        case OP_LT: printf(" LT "); break;
-        case OP_LE: printf(" LE "); break;
-        case OP_GT: printf(" GT "); break;
-        case OP_GE: printf(" GE "); break;
-        case OP_EQ: printf(" EQ "); break;
-        case OP_NE: printf(" NE "); break;
+        case OP_LT: fprintf(f, " LT "); break;
+        case OP_LE: fprintf(f, " LE "); break;
+        case OP_GT: fprintf(f, " GT "); break;
+        case OP_GE: fprintf(f, " GE "); break;
+        case OP_EQ: fprintf(f, " EQ "); break;
+        case OP_NE: fprintf(f, " NE "); break;
     }
     return;
 }
 
 
-static void print_expression(struct expression *e)
+static void print_expression(FILE *f, struct expression *e)
 {
-    printf("(");
+    fprintf(f, "(");
     switch (e->type)
     {
         case EXPR_LITERAL_INT:
-            printf("%lld", e->idata);
+            fprintf(f, "%lld", e->idata);
             break;
         case EXPR_LITERAL_STRING:
-            printf("\"%s\"", (char *)e->pdata);
+            fprintf(f, "\"%s\"", (char *)e->pdata);
             break;
         case EXPR_LITERAL_ARRAY:
-            printf("<type at %p>:", e->pdata); print_expression(e->childs[0]);
+            fprintf(f, "<type at %p>:", e->pdata); print_expression(f, e->childs[0]);
             break;
         case EXPR_ARRAY_INDEX:
-            print_expression(e->childs[0]); printf("["); print_expression(e->childs[1]); printf("]");
+            print_expression(f, e->childs[0]); fprintf(f, "["); print_expression(f, e->childs[1]); fprintf(f, "]");
             break;
         case EXPR_VARIABLE:
-            printf("%s", ((struct variable *)e->pdata)->name);
+            fprintf(f, "%s", ((struct variable *)e->pdata)->name);
             break;
         case EXPR_QUERY:
-            printf("?"); print_expression(e->childs[0]);
+            fprintf(f, "?"); print_expression(f, e->childs[0]);
             break;
         case EXPR_PUSH:
-            print_expression(e->childs[0]); printf(" <- "); print_expression(e->childs[1]);
+            print_expression(f, e->childs[0]); fprintf(f, " <- "); print_expression(f, e->childs[1]);
             break;
         case EXPR_OP:
-            if (e->childs[0] != NULL){ print_expression(e->childs[0]);}
-            print_op((enum op_type)e->idata);
-            if (e->childs[1] != NULL){ print_expression(e->childs[1]);}
+            if (e->childs[0] != NULL){ print_expression(f, e->childs[0]);}
+            print_op(f, (enum op_type)e->idata);
+            if (e->childs[1] != NULL){ print_expression(f, e->childs[1]);}
             break;
     }
-    printf(")");
+    fprintf(f, ")");
 }
 
-static void print_code_indented(struct block *b, int indent)
+static void print_code_indented(FILE *f, struct block *b, int indent)
 {
-    #define P {for (int i = 0; i < indent; ++i) putchar(' ');}
-    P printf("Block %p ", b); printf("[parent %p]\n", b->parent);
+    #define P {for (int i = 0; i < indent; ++i) putc(' ', f);}
+    P fprintf(f, "Block %p ", b); fprintf(f, "[parent %p]\n", b->parent);
     for (int i = 0; i < b->locals_len; ++i)
     {
-        P printf("local %2d: ", i); print_variable(b->locals[i]);
-        printf("\n");
+        P fprintf(f, "local %2d: ", i); print_variable(f, b->locals[i]);
+        fprintf(f, "\n");
     }
-    P printf("...\n");
+    P fprintf(f, "...\n");
     for (int i = 0; i < b->code_len; ++i)
     {
         switch (b->code[i]->type)
         {
             case STMT_BLOCK:
-                P printf("Block:\n");
-                print_code_indented(b->code[i]->block, indent + 4);
+                P fprintf(f, "Block:\n");
+                print_code_indented(f, b->code[i]->block, indent + 4);
                 break;
             case STMT_LOOP:
-                P printf("Loop: guard: "); print_expression(b->code[i]->loop.expr); printf("\n");
-                P printf("Block:\n");
-                print_code_indented(b->code[i]->loop.block, indent + 4);
+                P fprintf(f, "Loop: guard: "); print_expression(f, b->code[i]->loop.expr); fprintf(f, "\n");
+                P fprintf(f, "Block:\n");
+                print_code_indented(f, b->code[i]->loop.block, indent + 4);
                 break;
             case STMT_MATCH:
-                P printf("Match: guard: "); print_expression(b->code[i]->match.expr); printf("\n");
-                P printf("cases:\n");
+                P fprintf(f, "Match: guard: "); print_expression(f, b->code[i]->match.expr); fprintf(f, "\n");
+                P fprintf(f, "cases:\n");
                 for (int j = 0; j < b->code[i]->match.cases_len; ++j)
                 {
                     P 
-                    printf("case <"); 
+                    fprintf(f, "case <"); 
                     if (b->code[i]->match.cases_default[j] != NULL)
                     {
-                        printf("default:");
-                        print_variable(b->code[i]->match.cases_default[j]);
+                        fprintf(f, "default:");
+                        print_variable(f, b->code[i]->match.cases_default[j]);
                     }
                     else
                     {
-                        print_expression(b->code[i]->match.cases_literal[j]);
+                        print_expression(f, b->code[i]->match.cases_literal[j]);
                     }
                     if (b->code[i]->match.cases_guard[j] != NULL)
                     {
-                        printf("> where <"); 
+                        fprintf(f, "> where <"); 
                         {
-                            print_expression(b->code[i]->match.cases_guard[j]);
+                            print_expression(f, b->code[i]->match.cases_guard[j]);
                         }
                     }
-                    printf("> then\n"); 
-                    print_code_indented(b->code[i]->match.cases_block[j], indent + 4);
+                    fprintf(f, "> then\n"); 
+                    print_code_indented(f, b->code[i]->match.cases_block[j], indent + 4);
                 }
                 break;
             case STMT_EXPRESSION:
-                P printf("Expr:"); print_expression(b->code[i]->expr); printf("\n");
+                P fprintf(f, "Expr:"); print_expression(f, b->code[i]->expr); fprintf(f, "\n");
                 break;
             case STMT_WORKER:
-                P printf("Call: %s\n", b->code[i]->worker.worker->name);
+                P fprintf(f, "Call: %s\n", b->code[i]->worker.worker->name);
                 break;
             case STMT_BREAK:
-                P printf("Break.\n");
+                P fprintf(f, "Break.\n");
                 break;
         }
     }
     #undef P
 }
 
-static void print_code(struct block *b)
+static void print_code(FILE *f, struct block *b)
 {
-    print_code_indented(b, 4);
+    print_code_indented(f, b, 4);
 }
 
-static void print_worker(struct worker *w)
+static void print_worker(FILE *f, struct worker *w)
 {
-    printf("Worker %s\n", w->name);
+    fprintf(f, "Worker %s\n", w->name);
     for (int i = 0; i < w->inputs_len; ++i)
     {
-        printf("Input  %2d: ", i);
-        print_variable(w->inputs[i]);
-        printf("\n");
+        fprintf(f, "Input  %2d: ", i);
+        print_variable(f, w->inputs[i]);
+        fprintf(f, "\n");
     }
     for (int i = 0; i < w->outputs_len; ++i)
     {
-        printf("Output %2d: ", i);
-        print_variable(w->outputs[i]);
-        printf("\n");
+        fprintf(f, "Output %2d: ", i);
+        print_variable(f, w->outputs[i]);
+        fprintf(f, "\n");
     }
-    printf("\n");
-    print_code(w->body);
-    printf("\n");
-    printf("\n");
+    fprintf(f, "\n");
+    print_code(f, w->body);
+    fprintf(f, "\n");
+    fprintf(f, "\n");
 }
 
 int export_program_json(struct program *program, const char *filename)
 {
-    for (int i = 0; i < program->workers_len; ++i)
+    /* export types */
+    FILE *f = fopen(filename, "w");
+    fprintf(f, "{");
     {
-        print_worker(program->workers[i]);
+        fprintf(f, "\"types\":{");
+        for (int i = 0; i < program->types_len; ++i)
+        {
+            if (i != 0) { putc(',', f); }
+            print_type(f, program->types[i]);
+        }
+        fprintf(f, "}");
     }
+    fprintf(f, "}");
+    fclose(f);
     return 0;
 }
